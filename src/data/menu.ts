5963c4c4591cefd84dce853a21f232c4fb78ddf9
Irwanto Huang <irwanto.ng11@gmail.com>
2024-06-25 23:39:58 +0700

    edit menu icon

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -3,8 +3,8 @@ import { Menu } from "../types/Menu";
 const initMenu = (): Menu[] => {
     return [
         { name: 'Home', link: '/home', icon: 'fa-solid fa-home' },
-        { name: 'About', link: '/about', icon: 'fa-solid fa-home' },
-        { name: 'Content', link: '/content', icon: 'fa-solid fa-home' },
+        { name: 'About', link: '/about', icon: 'fa-solid fa-circle-info' },
+        { name: 'Content', link: '/content', icon: 'fa-solid fa-play' },
     ];
 };
 
