3a325a7ae5fe284c7bd5a9424a1d352bb8ca0e42
Irwanto Huang <irwanto.ng11@gmail.com>
2024-06-28 14:03:27 +0700

    FIX Hero Image Position

--- src/components/Home/Hero.vue
+++ src/components/Home/Hero.vue
@@ -1,11 +1,5 @@
 <template>
-    <div class="h-[250px] lg:h-[400px] w-full bg-red-100 overflow-hidden"> 
-        <img src="./../../assets/background/hero.jpg" alt="Waseda Boys World Trip" class="w-full h-full object-cover lg:object-cover hero-img ">
+    <div class="h-[250px] lg:h-[400px] w-full overflow-hidden"> 
+        <img src="./../../assets/background/hero.jpg" alt="Waseda Boys World Trip" class="w-full h-full object-cover object-center lg:object-[0_20%] ">
     </div>
 </template>
-
-<style scoped>
-.hero-img {
-    object-position: 0 20%;
-}
-</style>
